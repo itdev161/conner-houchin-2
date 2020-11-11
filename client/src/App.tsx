c17a4f99e2173775772e4895d1d6ed3d415454c8
houchinc <60678107+houchinc@users.noreply.github.com>
2020-11-11 12:02:33 -0600

    completed activity 7

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import axios from 'axios';
 import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Register from './components/Register/register';
+import Login from './components/Login/Login';
 
 class App extends React.Component {
   state = {
@@ -43,12 +45,8 @@ class App extends React.Component {
               {this.state.data}
             </Route>
             <Switch>
-              <Route path="/register">
-                Register
-              </Route>
-              <Route path="/login">
-                Login
-              </Route>
+              <Route exact path="/register" component={Register}/>
+              <Route exact path="/login" component={Login}/>
             </Switch>
           </main>
         </div>
